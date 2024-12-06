e1c662b3f947488748bd8ff0d648b1df38b87ed5
Mateusz Kubiak <matkubiak888@gmail.com>
2024-12-06 02:40:23 +0100

    feat: labels module

---
+++ src/labels.py
@@ -0,0 +1,12 @@
+import csv
+
+def load_labels(path):
+    with open(path, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        parsed_data = [row for row in reader]
+
+    header = parsed_data[0]
+    rows = parsed_data[1:]
+
+    mos_values = [float(row[1]) for row in rows]
+    return mos_values
