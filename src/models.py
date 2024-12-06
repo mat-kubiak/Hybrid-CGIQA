c5a69537ef669c6bc58557b5e1b479805d2afddf
Mateusz Kubiak <matkubiak888@gmail.com>
2024-12-06 02:40:48 +0100

    feat: models module

---
+++ src/models.py
@@ -0,0 +1,38 @@
+from tensorflow import keras
+
+def init_model(max_height, max_width):
+    model = keras.Sequential([
+        keras.layers.Input(shape=(max_height, max_width, 3)),
+        keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+        keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+        keras.layers.GlobalAveragePooling2D(),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(1, activation="linear"),
+    ])
+
+    model.compile(
+        optimizer = keras.optimizers.Adam(),
+        loss = keras.losses.MeanSquaredError(),
+        metrics=["mae"]
+    )
+
+    return model
+
+def train_model(model, x_train, y_train, epochs, batch_size):
+    model.fit(
+        x=x_train,
+        y=y_train,
+        verbose=1,
+        epochs=epochs,
+        batch_size=batch_size,
+    )
+    return model
+
+def save_model(model, path):
+    if model is None:
+        raise ValueError("Model has not been created.")
+    
+    model.save(path)
