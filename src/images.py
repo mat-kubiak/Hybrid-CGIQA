dd9bf2d4f63f9d7e24b9ee54b05153cb03f016bd
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-20 00:07:59 +0100

    preparing and previewing images

--- src/images.py
+++ src/images.py
@@ -1,5 +1,6 @@
-import cv2
+import cv2, os, math
 import tensorflow as tf
+import numpy as np
 
 def _load_img_cv(path):
     image = cv2.imread(path)
@@ -12,8 +13,42 @@ def _load_img_cv(path):
 def resize_image(image, target_width, target_height):
     return cv2.resize(image, (target_width, target_height))
 
+def preview_img(image):
+    if image.dtype != np.uint8:
+        image = (image * 255).astype(np.uint8)
+
+    cv2.imshow("image", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    cv2.waitKey(0)
+
 def prepare_img_for_size(image, target_width, target_height):
     h, w, c = image.shape
+
+    # exact match
+    if h == target_height and w == target_width:
+        return image
+
+    # too big
+    if h > target_height or w > target_width:
+        aaa = target_width / target_height
+        bbb = w / h
+
+        new_width = target_width
+        new_height = target_height
+
+        if (bbb < aaa): # higher, preserve height
+            new_width = math.floor(bbb * target_height)
+
+        if (bbb > aaa): # wider, preserve height
+            new_height = math.floor(target_width / bbb)
+        
+        resized = resize_image(image, new_width, new_height)
+        padded = pad_image(resized, target_width, target_height)
+
+        return padded
+    
+    # too small
+    return pad_image(image, target_width, target_height)
+
 
 def pad_image(image, target_width, target_height):
     h, w, c = image.shape
@@ -42,3 +77,6 @@ def _load_img_tf(path):
 
 def load_img(path):
     return _load_img_cv(path)
+
+def get_image_list(path):
+    return np.sort(np.array(os.listdir(path)))
