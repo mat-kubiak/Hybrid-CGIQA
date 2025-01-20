2cb4871e4b517d8214f75d7edb2f70a9b94a8aad
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-21 00:05:10 +0100

    checktf updated

--- tools/checktf.py
+++ tools/checktf.py
@@ -5,24 +5,40 @@ import matplotlib.pyplot as plt
 project_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(project_dir)
 
-def load_image(image_path):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    return image
-
-def save_image(image, output_path):
-    encoded_image = tf.image.encode_jpeg(image)
-    tf.io.write_file(output_path, encoded_image)
+MAX_HEIGHT = 1080
+MAX_WIDTH = 1920
 
 input_image_path = f'{project_dir}/data/images/train/movie_1488.jpg'
 output_image_path = f'{project_dir}/output.jpg'
 
-image = load_image(input_image_path)
+def load_image(path):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0
 
-target_height = 1080
-target_width = 1920
+    height, width = tf.shape(image)[0], tf.shape(image)[1]
 
-padded_image = tf.image.resize_with_pad(image, target_height, target_width)
-padded_image = tf.cast(padded_image, tf.uint8)
+    # the same
+    if height == MAX_HEIGHT and width == MAX_WIDTH:
+        return image
 
-save_image(padded_image, output_image_path)
+    # smaller - only pad
+    if height <= MAX_HEIGHT and width <= MAX_WIDTH:
+        image = tf.image.resize_with_crop_or_pad(image, MAX_HEIGHT, MAX_WIDTH)
+        return image
+
+    # bigger in 1 or 2 dims - resize and pad
+    image = tf.image.resize_with_pad(image, MAX_HEIGHT, MAX_WIDTH)
+    return image
+
+def save_image(image, output_path):
+    image = tf.cast(image * 255.0, tf.uint8)
+    encoded_image = tf.image.encode_jpeg(image)
+    tf.io.write_file(output_path, encoded_image)
+
+def main():
+    image = load_image(input_image_path)
+    save_image(image, output_image_path)
+
+if __name__ == '__main__':
+    main()
