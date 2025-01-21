1bcd57b3f87587a4c9675391d57c15b5ff978375
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-21 02:15:00 +0100

    save and load test data

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ DATA_PATH = f'{project_dir}/data'
 MODEL_PATH = f'{project_dir}/model.keras'
 MOS_PATH = f'{DATA_PATH}/mos.csv'
 IMG_DIRPATH = f'{DATA_PATH}/images/test'
+NPY_SAVEFILE = f'{project_dir}/test_data.npy'
 
 MAX_HEIGHT = 1080
 MAX_WIDTH = 1920
@@ -40,19 +41,22 @@ def main():
     image_paths = images.get_image_list(IMG_DIRPATH)
     image_paths = IMG_DIRPATH + "/" + image_paths
 
-    print(image_paths.shape)
-    print(mos.shape)
-
     if not models.model_exists(MODEL_PATH):
         print("Fatal error: no model found")
         sys.exit(1)
     model = models.load_model(MODEL_PATH)
 
-    dataset = tf.data.Dataset.from_tensor_slices((image_paths, mos))
-    dataset = dataset.map(load_img)
-    dataset = dataset.batch(TEST_BATCH_SIZE)
+    predictions = None
+    if os.path.isfile(NPY_SAVEFILE):
+        predictions = np.load(NPY_SAVEFILE)
+    else:
+        dataset = tf.data.Dataset.from_tensor_slices((image_paths, mos))
+        dataset = dataset.map(load_img)
+        dataset = dataset.batch(TEST_BATCH_SIZE)
 
-    predictions = model.predict(dataset)
+        predictions = model.predict(dataset)
+        np.save(NPY_SAVEFILE, predictions)
+    
     predicted_categories = np.argmax(predictions, axis=1)
     true_labels_category = np.argmax(mos, axis=1)
 
