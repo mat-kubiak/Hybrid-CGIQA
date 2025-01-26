6718b460060ef7c0d961962233209ac8775be5cf
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-26 21:53:04 +0100

    xla compatible histogram

--- src/models.py
+++ src/models.py
@@ -3,6 +3,26 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+# XLA-compatible impl of tf.histogram_fixed_width()
+def _compute_histogram(values, value_range, nbins=256):
+    min_val, max_val = value_range
+    
+    # Prevent division by zero
+    bin_width = tf.maximum(
+        (max_val - min_val) / tf.cast(nbins, values.dtype), 
+        tf.keras.backend.epsilon()
+    )
+    
+    # Compute bin indices
+    bins = tf.floor((values - min_val) / bin_width)
+    bins = tf.cast(tf.clip_by_value(bins, 0, nbins - 1), tf.int32)
+    
+    # One-hot encode and sum
+    encoded = tf.one_hot(bins, depth=nbins, dtype=tf.int32)
+    histogram = tf.reduce_sum(encoded, axis=0)
+    
+    return histogram
+
 class NormalizedHistogram(tf.keras.layers.Layer):
     def __init__(self, nbins=256, **kwargs):
         super().__init__(**kwargs)
@@ -16,7 +36,7 @@ class NormalizedHistogram(tf.keras.layers.Layer):
                 channel = image[..., c]
                 channel = tf.reshape(channel, [-1])
 
-                hist = tf.histogram_fixed_width(channel, value_range=[0.0, 1.0], nbins=self.nbins)
+                hist = _compute_histogram(channel, value_range=[0.0, 1.0], nbins=self.nbins)
                 hist = tf.cast(hist, tf.float32)
                 hist = hist / tf.reduce_sum(hist)
 
