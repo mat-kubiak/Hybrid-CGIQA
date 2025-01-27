d1d46eb2fc96401e1a965600049ff91b114ba200
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-27 23:01:37 +0100

    remove deprecated

--- src/images.py
+++ src/images.py
@@ -4,7 +4,6 @@ import numpy as np
 
 def augment_image(image):
     image = tf.image.random_flip_left_right(image)
-    image = tfa.image.rotate(image, angles=tf.random.uniform([], -0.1, 0.1))
 
     image = tf.image.random_brightness(image, max_delta=0.2)  # Brightness change up to ±0.2
     image = tf.image.random_contrast(image, lower=0.8, upper=1.2)  # Contrast factor [0.8, 1.2]
@@ -13,8 +12,6 @@ def augment_image(image):
 
     noise = tf.random.normal(shape=tf.shape(image), mean=0.0, stddev=0.05)
     image = image + noise
-
-    image = tfa.image.gaussian_filter2d(image, filter_shape=(3, 3))
     
     return tf.clip_by_value(image, 0.0, 1.0)
 
