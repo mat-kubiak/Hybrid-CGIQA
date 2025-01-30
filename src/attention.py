b8828c444197fd2127f26e96772fc66d8824f7d1
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-30 16:00:23 +0100

    refactored spatial attention layer

--- src/attention.py
+++ src/attention.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Conv2D, Activation, Multiply, Add, GlobalMaxPooling2D, GlobalAveragePooling2D, Reshape, Concatenate
+from tensorflow.keras import layers
 
-class SpatialAttention(Layer):
+class SpatialAttention(layers.Layer):
     def __init__(self, kernel_size=7, **kwargs):
         super(SpatialAttention, self).__init__(**kwargs)
         self.kernel_size = kernel_size
 
     def build(self, input_shape):
-        self.conv = Conv2D(filters=1,
+        self.conv = layers.Conv2D(filters=1,
                            kernel_size=self.kernel_size,
                            strides=1,
                            padding="same",
@@ -15,33 +15,17 @@ class SpatialAttention(Layer):
         super(SpatialAttention, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
-        """
-        Forward pass of the layer.
-
-        Args:
-            inputs (tf.Tensor): Input feature map of shape (batch_size, height, width, channels).
-
-        Returns:
-            tf.Tensor: Output feature map after applying spatial attention.
-        """
-        # Compute average pooling and max pooling along the channel axis
-        avg_pool = tf.reduce_mean(inputs, axis=-1, keepdims=True)  # Shape: (batch, H, W, 1)
-        max_pool = tf.reduce_max(inputs, axis=-1, keepdims=True)  # Shape: (batch, H, W, 1)
+        avg_pool = tf.reduce_mean(inputs, axis=-1, keepdims=True)
+        max_pool = tf.reduce_max(inputs, axis=-1, keepdims=True)
         
-        # Concatenate average and max pooling results along the channel axis
-        concat = Concatenate(axis=-1)([avg_pool, max_pool])  # Shape: (batch, H, W, 2)
+        concat = Concatenate(axis=-1)([avg_pool, max_pool])
         
-        # Generate the spatial attention map using a convolution layer
-        attention_map = self.conv(concat)  # Shape: (batch, H, W, 1)
+        attention_map = self.conv(concat)
 
-        # Multiply the attention map with the input feature map
-        output = Multiply()([inputs, attention_map])
+        output = layers.Multiply()([inputs, attention_map])
         return output
 
     def get_config(self):
-        """
-        Returns the configuration of the layer for serialization.
-        """
         config = super(SpatialAttention, self).get_config()
         config.update({"kernel_size": self.kernel_size})
         return config
