645f89bc67966c69da71e40cbb03a8711a41fb1f
Mateusz Kubiak <matkubiak888@gmail.com>
2025-01-31 23:08:44 +0100

    remove csv history dumping feature

--- src/tracker.py
+++ src/tracker.py
@@ -1,13 +1,5 @@
 import os, datetime, configparser
 
-def _get_last_line(path):
-    with open(path, "rb") as f:
-        f.seek(-2, 2)
-        while f.read(1) != b"\n":
-            f.seek(-2, 1)
-        last_line = f.readline().decode()
-    return last_line
-
 class Tracker:
 
     def __init__(self, output_dir):
@@ -17,7 +9,6 @@ class Tracker:
 
         self.batch = 0
         self.epoch = 0
-        self.batch_counter = 0
 
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
@@ -55,15 +46,3 @@ class Tracker:
     def logprint(self, message):
         print(message)
         self.log(message)
-
-    def append_csv_history(self, stats):
-        keys = stats.keys()
-        values = list(map(str, stats.values()))
-
-        isfile = os.path.isfile(self.history_path)
-        with open(self.history_path, 'a') as file:
-            if not isfile:
-                file.write('batch,' + ','.join(keys) + '\n')
-            file.write(str(self.batch_counter) + ',' + ','.join(values) + '\n')
-        
-        self.batch_counter += 1
