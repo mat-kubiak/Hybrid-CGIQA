29a8df39a749888c8b88b2fff82bb8e3c2f778f3
Mateusz Kubiak <matkubiak888@gmail.com>
2025-02-06 21:51:38 +0100

    inception + spp invariable to input shape

--- src/models.py
+++ src/models.py
@@ -45,6 +45,32 @@ def get_sample_weights(labels, power=1.0):
     weights = weights / tf.reduce_mean(weights)
     return weights
 
+def LightInceptionModule(x, filters_1x1, filters_3x3, filters_5x5):
+    conv1x1 = layers.Conv2D(filters_1x1, (1,1), padding='same', activation='relu')(x)
+    
+    conv3x3 = layers.Conv2D(filters_3x3, (3,3), padding='same', activation='relu')(x)
+    
+    conv5x5 = layers.Conv2D(filters_5x5, (5,5), padding='same', activation='relu')(x)
+    
+    pool = layers.MaxPooling2D((3,3), strides=(1,1), padding='same')(x)
+    pool_proj = layers.Conv2D(filters_1x1, (1,1), padding='same', activation='relu')(pool)
+    
+    return layers.Concatenate(axis=-1)([conv1x1, conv3x3, conv5x5, pool_proj])
+
+def _spp(x):
+    spp_1 = layers.AveragePooling2D(pool_size=(x.shape[1], x.shape[2]))(x)
+    spp_2 = layers.AveragePooling2D(pool_size=(x.shape[1]//2, x.shape[2]//2), strides=(x.shape[1]//2, x.shape[2]//2))(x)
+    spp_4 = layers.AveragePooling2D(pool_size=(x.shape[1]//4, x.shape[2]//4), strides=(x.shape[1]//4, x.shape[2]//4))(x)
+    spp_8 = layers.AveragePooling2D(pool_size=(x.shape[1]//8, x.shape[2]//8), strides=(x.shape[1]//8, x.shape[2]//8))(x)
+    
+    spp_1 = layers.Flatten()(spp_1)
+    spp_2 = layers.Flatten()(spp_2)
+    spp_4 = layers.Flatten()(spp_4)
+    spp_8 = layers.Flatten()(spp_8)
+    
+    x = layers.Concatenate()([spp_1, spp_2, spp_4, spp_8])
+    return x
+
 def _hidden_layers(input_layer):
     
     # hist route
@@ -55,29 +81,20 @@ def _hidden_layers(input_layer):
     h = layers.Dropout(0.4)(h)
 
     # conv route
-    c = layers.Conv2D(kernel_size=(3,3), filters=32, kernel_regularizer=l2(1e-5))(input_layer)
-    c = layers.Conv2D(kernel_size=(3,3), filters=32, kernel_regularizer=l2(1e-5))(c)
-    c = SpatialAttention()(c)
-    c = layers.AveragePooling2D(pool_size=(3,3))(c)
-    c = layers.Conv2D(kernel_size=(3,3), filters=64, kernel_regularizer=l2(1e-5))(c)
-    c = layers.Conv2D(kernel_size=(3,3), filters=64, kernel_regularizer=l2(1e-5))(c)
-    c = SpatialAttention()(c)
-    c = layers.AveragePooling2D(pool_size=(3,3))(c)
-
-    c = layers.Conv2D(kernel_size=(3,3), filters=128, kernel_regularizer=l2(1e-5))(c)
-    c = layers.Conv2D(kernel_size=(3,3), filters=128, kernel_regularizer=l2(1e-5))(c)
-    c = SpatialAttention()(c)
-    c = layers.AveragePooling2D(pool_size=(3,3))(c)
-
-    c = layers.Conv2D(kernel_size=(1,1), filters=1, activation="linear", kernel_regularizer=l2(1e-5))(c)
-    c = layers.Flatten()(c)
-
-    c = layers.Dense(units=512, activation="relu", kernel_regularizer=l2(1e-4))(c)
+    c = layers.Conv2D(16, (7,7), strides=(2,2), padding='same', activation='relu')(input_layer)
+    c = layers.MaxPooling2D((3,3), strides=(2,2), padding='same')(c)
+    
+    c = LightInceptionModule(c, 8, 16, 8)
+    c = LightInceptionModule(c, 16, 24, 16)
+    c = layers.MaxPooling2D((3,3), strides=(2,2), padding='same')(c)
+    
+    c = _spp(c)
+    c = layers.Dense(units=256, activation="relu", kernel_regularizer=l2(1e-4))(c)
     c = layers.Dropout(0.4)(c)
 
     # merge
     x = layers.Concatenate()([h, c])
-    x = layers.Dense(units=512, activation="relu", kernel_regularizer=l2(1e-4))(x)
+    x = layers.Dense(units=256, activation="relu", kernel_regularizer=l2(1e-4))(x)
     x = layers.Dense(units=256, activation="relu", kernel_regularizer=l2(1e-4))(x)
     x = layers.Dropout(0.4)(x)
 
