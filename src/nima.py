1ad08ddf80f82820b9c150d64e8c4030ef921c72
Mateusz Kubiak <matkubiak888@gmail.com>
2025-02-10 05:04:28 +0100

    refactor: improve nima.py clarity

--- src/nima.py
+++ src/nima.py
@@ -1,18 +1,19 @@
-import os, sys
+import sys
+from pathlib import Path
 
-VENDOR_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'vendor')
-WEIGHTS_PATH = os.path.join(VENDOR_PATH, 'weights_mobilenet_aesthetic_0.07.hdf5')
+VENDOR_PATH = Path(__file__).parent.parent / 'vendor'
+WEIGHTS_PATH = VENDOR_PATH / 'weights_mobilenet_aesthetic_0.07.hdf5'
 
-sys.path.append(VENDOR_PATH)
+sys.path.append(str(VENDOR_PATH))
 from nima_model_builder import Nima
 
 def load_pretrained_nima():
     nima = Nima(base_model_name="MobileNet", weights=None)
     nima.build()
 
-    if not os.path.isfile(WEIGHTS_PATH):
+    if not WEIGHTS_PATH.is_file():
         raise FileNotFoundError(f'Weights for NIMA not found at: {WEIGHTS_PATH}')
 
-    nima.nima_model.load_weights(WEIGHTS_PATH)
+    nima.nima_model.load_weights(str(WEIGHTS_PATH))
 
     return nima.nima_model
