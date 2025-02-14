1578f3b7dd760fa49992105f2fed3676321ae498
Mateusz Kubiak <matkubiak888@gmail.com>
2025-02-14 09:02:26 +0100

    update summary tool

--- tools/summary.py
+++ tools/summary.py
@@ -10,8 +10,8 @@ import models
 
 MODEL_PATH = f''
 
-HEIGHT = 512
-WIDTH = 512
+HEIGHT = 224
+WIDTH = 224
 
 def main():
     if len(MODEL_PATH) != 0:
@@ -19,17 +19,26 @@ def main():
     else:
         model = models.init_model_continuous(HEIGHT, WIDTH)
 
-    plot_model(model, to_file="arch.png", show_shapes=True, show_dtype=True, show_layer_names=True, show_trainable=True)
-
     model.summary()
-    print(f"Optimizer: \n{json.dumps(model.optimizer.get_config(), indent=4)}")
+
+    try:
+        plot_model(
+            model,
+            to_file="arch.png",
+            show_shapes=True,
+            show_dtype=True,
+            show_layer_names=True,
+            show_trainable=True
+        )
+    except Exception as e:
+        print(f'Error while plotting the model: {e}')
+
+    print(f"Hidden layer count: {len(model.layers)-2}")
     print(f"Loss function: {model.loss.name}")
     print(f"Metrics: {[metric.name for metric in model.metrics]}")
     print(f"Input shape {model.input_shape}")
     print(f"Output shape {model.compute_output_shape(model.input_shape)}")
-
-    layer_cfg = [filter_cfg(layer.get_config()) for layer in model.layers]
-    # print(json.dumps(layer_cfg, indent=2))
+    print(f"Optimizer: \n{json.dumps(model.optimizer.get_config(), indent=4)}")
 
 if __name__ == '__main__':
     main()
